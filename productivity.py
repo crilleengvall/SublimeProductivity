43863234465bf8927baea1f9197d5c3d06415bdf
Christian Engvall <crilleengvall@gmail.com>
2016-05-12 16:02:55 +0200

    Updating tweet output to match PEP8 convention

--- productivity.py
+++ productivity.py
@@ -28,7 +28,8 @@ class ProductivityCommand(sublime_plugin.TextCommand):
 		active_view = current_window.active_view()
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token, access_token_secret)
+		active_view.insert(edit, 0, 'class Twitter(): \n')
 		api = tweepy.API(auth)
 		public_tweets = api.home_timeline()
 		for tweet in public_tweets:
-			active_view.insert(edit, 0, 'class ' + tweet.user.name + '(handle = @' + tweet.user.screen_name + '): \n\ttext = "' + tweet.text + '"\n\n')
+			active_view.insert(edit, active_view.size(), '    #time: ' + str(tweet.created_at) + '\n    #retweeted: ' + str(tweet.retweet_count) + '\n    #favorites: ' + str(tweet.favorite_count) + '    \n    def ' + tweet.user.name.lower().replace(' ', '_') + '(handle = "@' + tweet.user.screen_name + '"): \n        text = "' + tweet.text + '"\n\n')
